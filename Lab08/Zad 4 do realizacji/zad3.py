1ca7827cb4d18bc8a4080e47fd2741f144e1de08
ExaggeratedRumors <exaggeratedrumors@gmail.com>
2021-12-09 15:50:46 +0100

    Lab10

--- Lab08/Zad 4 do realizacji/zad3.py
+++ Lab08/Zad 4 do realizacji/zad3.py
@@ -1,3 +1,48 @@
 # 3. Zapisywanie trzech najlepszych wyników gry (np. rzucanie dwoma kostkami aż wyrzucimy 6,6) 
 # do pliku score.txt i przechowujemy ilości rzutów w danej sesji, datę i czas w postaci YYYY-MM-DD HH24:MIN:SEC. 
 # Jeżeli wynik jest taki sam a data jest starsza to nie zastępujemy jej.
+
+import datetime
+import random
+
+
+def add_result(score, scores):
+    new_score = [str(score), str(datetime.datetime.today())]
+    if not scores:
+        scores.append(new_score)
+    else:
+        for i in range(0, len(scores)):
+            if score < int(scores[i][0]):
+                if len(scores) > 2:
+                    scores.pop()
+                scores.insert(i, new_score)
+                break
+
+
+def save_score(score):
+    with open("score.txt", "r") as file:
+        scores = [[val for val in line.split()] for line in file]
+    with open("score.txt", "w") as file:
+        add_result(score, scores)
+        for s in scores:
+            separator = " "
+            output = separator.join(s)
+            file.write(output + "\n")
+
+
+def dice_throw(number_of_throws):
+    output = []
+    for i in range(0, number_of_throws):
+        output.append(random.randint(1, 6))
+    return output
+
+
+counter = 0
+while True:
+    result = dice_throw(2)
+    if result == [6, 6]:
+        break
+    counter += 1
+
+print("Wynik:", counter)
+save_score(counter)
--- Lab08/Zad 4 do realizacji/zad4.py
+++ Lab08/Zad 4 do realizacji/zad4.py
@@ -1,2 +1,20 @@
 # 4. Zapisujemy do pliku kąt, sinus i consinus z przedziału 0,360 co 1 stopień oraz czytamy te dane.
 # przy zapisie stosujemy różne sposoby formatowania łańcucha znaków ( %, .format() oraz f'') ale efekt ma być taki sam.
+
+import math
+
+start_point = 0
+end_point = 360
+
+with open("sinus_cosinus.txt", "w") as file:
+    for i in range(start_point, end_point + 1):
+        file.write('%3d ' % i)
+        file.write("{0:0.5f} ".format(math.sin(math.radians(i))))
+        file.write(f"{round(math.cos(math.radians(i)), 5)}")
+        file.write("\n")
+
+with open("sinus_cosinus.txt", "r") as file:
+    result = [[float(val) for val in line.split()] for line in file]
+
+for i in result:
+    print(i)
